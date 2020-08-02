9a8d68913eb244629b59455b5084798182391c46
SutrisDevPemula <sutris.devpemula@gmail.com>
2020-08-02 15:42:42 +0800

    fix khs

--- khs/app/Http/Resources/KhsResource.php
+++ khs/app/Http/Resources/KhsResource.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class KhsResource extends ResourceCollection
+class KhsResource extends JsonResource
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+    
     public function toArray($request)
     {
         return parent::toArray($request);
